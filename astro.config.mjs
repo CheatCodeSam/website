854bfaa13981e0dfe4b6744323267a4787d1433a
Jondolf <joona.m.aalto@outlook.com>
2022-10-29 16:47:12 +0300

    Add basic site structure
    - Added Sass for styling
    - Added "Projects" and "Blog" routes
    - Added nav bar
    - Changed homepage content
    - Blog posts added to src/pages/blog will show up on the Blog page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+
+function insertBlogLayoutPlugin() {
+  return function (_tree, file) {
+    file.data.astro.frontmatter.layout = file.cwd + '/src/layouts/PostLayout.astro';
+  };
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  markdown: {
+    remarkPlugins: [insertBlogLayoutPlugin],
+    extendDefaultPlugins: true,
+  },
 });
