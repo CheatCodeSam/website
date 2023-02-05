2aa6801c9bf5f60ea68c91ac9302533861f032e3
Jondolf <joona.m.aalto@outlook.com>
2023-02-05 21:30:38 +0200

    Fix content schemas

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,57 +1,39 @@
 import { z, defineCollection } from 'astro:content';
 
-const dateSchema = z.preprocess((arg) => {
-  if (typeof arg == "string" || arg instanceof Date) return new Date(arg);
-}, z.date());
-
 const blog = defineCollection({
-  slug: ({ defaultSlug }) => {
-    let parts = defaultSlug.split('/');
-    let year = parts[0];
-    let id = parts[1].split('-')[0];
-    let title = parts[1].slice(3);
-    return `${year}/${id}/${title}`;
-  },
-  schema: {
+  schema: z.object({
     id: z.number(),
-    url: z.string().optional(),
+    url: z.string().url().optional(),
     title: z.string(),
     description: z.string().optional(),
     tags: z.array(z.string()).optional(),
-    createdAt: dateSchema,
+    createdAt: z.string().or(z.date()).transform(str => new Date(str)),
     thumbnail: z.object({
       src: z.string(),
       alt: z.string(),
-      format: z.string().optional(),
-      width: z.number().optional(),
-      height: z.number().optional(),
-      aspectRatio: z.string().optional(),
-    }).optional(),
-  }
+      format: z.enum(['avif', 'jpeg', 'png', 'webp']).optional(),
+      width: z.number(),
+      height: z.number()
+    }).optional()
+  })
 });
 
 const projects = defineCollection({
-  slug: ({ defaultSlug }) => {
-    let id = defaultSlug.split('-')[0];
-    let title = defaultSlug.slice(3);
-    return `${id}/${title}`;
-  },
-  schema: {
+  schema: z.object({
     id: z.number(),
+    url: z.string().url().optional(),
     title: z.string(),
-    url: z.string().optional(),
     description: z.string().optional(),
     tags: z.array(z.string()).optional(),
-    createdAt: dateSchema,
+    createdAt: z.string().or(z.date()).transform(str => new Date(str)),
     thumbnail: z.object({
       src: z.string(),
       alt: z.string(),
-      format: z.string().optional(),
-      width: z.number().optional(),
-      height: z.number().optional(),
-      aspectRatio: z.string().optional(),
+      format: z.enum(['avif', 'jpeg', 'png', 'webp']).optional(),
+      width: z.number(),
+      height: z.number()
     }).optional(),
-  }
+  })
 });
 
 export const collections = {
