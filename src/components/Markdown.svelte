448237bc4070da85c78c6b4f503ac602b5a64759
Jondolf <joona.m.aalto@outlook.com>
2023-02-10 16:29:41 +0200

    Parse PageCard description markdown to HTML
    All block level elements are styled as inline, and images are disabled.

---
+++ src/components/Markdown.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { unified } from 'unified';
+  import remarkParse from 'remark-parse';
+  import remarkRehype from 'remark-rehype';
+  import remarkMath from 'remark-math';
+  import rehypeKatex from 'rehype-katex';
+  import rehypeStringify from 'rehype-stringify';
+
+  export let markdown: string;
+
+  $: htmlContent = unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(remarkMath)
+    .use(rehypeKatex)
+    .use(rehypeStringify)
+    .processSync(markdown)
+    .toString();
+</script>
+
+{@html htmlContent}
