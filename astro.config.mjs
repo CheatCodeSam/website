79bc652ed5b4a8cd71aebfa675244c3b3595dc1d
Jondolf <jondolf.dev@gmail.com>
2023-05-19 18:24:06 +0300

    Add Netlify adapter and astro-compress, update Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
 import image from '@astrojs/image';
 import svelte from '@astrojs/svelte';
+import compress from "astro-compress";
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import { remarkReadingTime } from './remark-plugins/remark-reading-time.mjs';
@@ -10,13 +11,14 @@ import { remarkReadingTime } from './remark-plugins/remark-reading-time.mjs';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [image(), svelte()],
+  adapter: netlify(),
+  integrations: [image(), svelte(), compress()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
-      wrap: true,
+      wrap: true
     },
     remarkPlugins: [remarkMath, remarkReadingTime],
-    rehypePlugins: [rehypeKatex],
-  },
+    rehypePlugins: [rehypeKatex]
+  }
 });
