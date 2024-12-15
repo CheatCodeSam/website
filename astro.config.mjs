54966b774ae2fe19f29f8232175bcb85c65e8e21
Joona Aalto <jondolf.dev@gmail.com>
2024-12-15 14:36:34 +0200

    Update dependencies and format

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,12 +8,14 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import { remarkReadingTime } from './remark-plugins/remark-reading-time.mjs';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
   site: 'https://joonaa.dev',
-  integrations: [svelte(), mdx(), sitemap(), partytown({ config: { forward: ['dataLayer.push'] } })],
+  integrations: [svelte(), mdx(), sitemap(), partytown({ config: { forward: ['dataLayer.push'] } }), icon()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
